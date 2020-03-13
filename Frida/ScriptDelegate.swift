f2f3f705ff1311b285f8715d7770876439fc10ae
Mike Runnals <mikerunnals@users.noreply.github.com>
2020-03-13 06:39:04 -0400

    Pass Raw Message Data from XPC to Mac App (#2)

--- Frida/ScriptDelegate.swift
+++ Frida/ScriptDelegate.swift
@@ -1,5 +1,10 @@
 @objc(FridaScriptDelegate)
 public protocol ScriptDelegate {
     @objc optional func scriptDestroyed(_ script: Script)
+    
+    /// option to recieve json serializer decode gum message as Any (objc objects)
     @objc optional func script(_ script: Script, didReceiveMessage message: Any, withData data: Data?)
+    
+    /// option to recieve non-decode gum message Data
+    @objc optional func script(_ script: Script, didReceiveMessageData message: Data, withData data: Data?)
 }
